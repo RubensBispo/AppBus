a1a46aa68cecaf9eb2b35660a6d40da388f1e744
Rubens Bispo <rubensbispo29@gmail.com>
2024-06-04 21:04:18 -0300

    "login"

--- app/src/main/java/br/com/etecia/appbus/SplashActivity.java
+++ app/src/main/java/br/com/etecia/appbus/SplashActivity.java
@@ -32,7 +32,7 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },2000);
+        },3000);
 
     }
 }
