6c86b5d1c22489314696f97b549b260167ef209f
Rubens Bispo <rubensbispo29@gmail.com>
2024-06-14 20:58:54 -0300

    validadndo login

--- app/src/main/java/br/com/etecia/appbus/LoginActivity.java
+++ app/src/main/java/br/com/etecia/appbus/LoginActivity.java
@@ -1,6 +1,8 @@
 package br.com.etecia.appbus;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edtEmail, edtSenha;
+    private Button btnEntrar, btnSair;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,10 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        edtEmail = findViewById(R.id.edtEmail);
+        edtSenha = findViewById(R.id.edtSenha);
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnSair = findViewById(R.id.btnSair);
     }
 }
